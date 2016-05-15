aa8ceab66e5547b6b4feb3502e6637a9a3b4a7b1
vandosant <sjskender@gmail.com>
2016-05-15 17:40:52 -0600

    Connect to mongodb and create test post with mongoose

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ let express = require('express')
 let app = express()
 let config = require('./config/config.js')
 
+let mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/test')
+
+let Post = mongoose.model('Post', {title: String})
+let firstPost = new Post({title: 'Another dev blurb'})
+firstPost.save(function (err) {
+  if (err) {
+    console.log(err)
+  }
+})
+
 app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`), function(err) {
