b0dc60b35fb7fa1b3ec9255fe2ef3fce06eb1cec
vandosant <sjskender@gmail.com>
2016-05-21 19:03:43 -0600

    Fetch all posts and log them

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('hello app')
+if (self.fetch) {
+  fetch('/api/posts')
+    .then(function (res) {
+        if (res.status === 200) {
+	  return res.json();
+	}
+    })
+    .then(function (json) {
+      console.log(json);
+    });
+}
