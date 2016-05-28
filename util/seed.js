d5bbeac1ee41bd72598712dbc98dfb141e437d35
vandosant <sjskender@gmail.com>
2016-05-27 23:32:19 -0600

    Add more posts to seed data

--- util/seed.js
+++ util/seed.js
@@ -5,7 +5,15 @@ const posts = [
   {title: 'Whip-por-will', content: `I got my window open in the southern cross hotel
 it's been my longest night I can tell
 by the way I'm not surprised
-to see the desert cover over paradise`}
+to see the desert cover over paradise`},
+  {title: 'North Star Blues', content: `Where were the rest of my songs tonight
+I only remember the North Star blues
+That simple old tune on the stage each night
+Marking the time that I lost you`},
+  {title: 'Blue Factory Flame', content: `When i die put my bones in an empty street to remind me of how it used to be.
+don’t write my name on a stone, bring a Coleman lantern and a radio,
+Cleveland game and two fishing poles and watch with me from the shore
+ghostly steel and iron ore ships coming home`}
 ]
 
 const createDocument = function(model, data) {
