016006f649a5a6c7d47502dd96cecfc2d35f42fe
vandosant <sjskender@gmail.com>
2016-05-29 22:07:42 -0600

    Refactoring variable names

--- api/user/userRoutes.js
+++ api/user/userRoutes.js
@@ -1,16 +1,16 @@
 let router = require('express').Router();
-let userModel = require('./userModel')
+let User = require('./userModel')
 const signToken = require('../../auth/auth').signToken;
 
 router.route('/')
   .get(function(req, res) {
-    userModel.find()
+    User.find()
     .then(function(doc) {
       res.json(doc);
     });
   })
   .post(function(req, res, next) {
-    userModel.create(req.body)
+    User.create(req.body)
     .then(function(user) {
       res.json({token: signToken(user._id)});
     }, function(err) {
