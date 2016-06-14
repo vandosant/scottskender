7cfa27d19259ebead0ce84afd955748c395e8c5c
vandosant <sjskender@gmail.com>
2016-06-13 18:55:03 -0600

    Create store on app to hold posts

--- client/Landing.jsx
+++ client/Landing.jsx
@@ -1,15 +1,11 @@
 const React = require('react')
 const Title = require('./Title')
-const Post = require('./postModel')
 
 let Landing = React.createClass({
-  getInitialState() {
-    return Post.load()
-  },
   render() {
     <div>
       <Title title='Musings' color='#000' />
-      {this.posts.map((post) => { return (
+      {this.props.posts.map((post) => { return (
 	<div key={post._id} className='panel'><a href='#'>{post.title}</a></div> )
       } )}
     </div>
--- client/app.js
+++ client/app.js
@@ -2,6 +2,7 @@ const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
 const {Router, Route, hashHistory} = require('react-router')
+const Posts = require('./postModel')
 
 const App = () => {
   return (
@@ -11,4 +12,10 @@ const App = () => {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+let store = Object.create(null)
+
+Posts.load()
+.then(function(posts) {
+  store = Object.assign(posts)
+  ReactDOM.render(<App store={store} />, document.getElementById('app'))
+})
